d95979b4ba5c46887443affa103ce7f33d2c2d9e
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-08-24 19:54:14 +0200

    feat: add basic eslint and prettier config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import eslintPlugin from "vite-plugin-eslint2";
+import prettierPlugin from "vite-plugin-prettier";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), eslintPlugin(), prettierPlugin()],
 });
