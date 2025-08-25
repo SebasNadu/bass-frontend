cd1f8054fd2e95f842f97eb873f7936f98eec72e
Alex <alexis.mar.schmidt@gmail.com>
2025-08-25 15:52:15 +0200

    feat: add components into login page

--- src/pages/public/login/Login.tsx
+++ src/pages/public/login/Login.tsx
@@ -1,12 +1,22 @@
-import LoginForm from './LoginForm'
-import SignUpForm from './SignUpForm'
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+import { useState } from "react";
 
 export default function Login() {
+  const [showSignUp, setShowSignUp] = useState(false);
 
-  return(
-    <>
-    <LoginForm/>
-    <SignUpForm/>
-    </>
-  )
+  const toggleForm = () => {
+    setShowSignUp((prev) => !prev);
+  };
+
+  return (
+    <div className="form-container">
+      <button onClick={toggleForm} className="toggle-button">
+        {showSignUp
+          ? "Already have an account? Log in"
+          : "Don’t have an account? Sign up"}
+      </button>
+      {showSignUp ? <SignUpForm /> : <LoginForm />}
+    </div>
+  );
 }
