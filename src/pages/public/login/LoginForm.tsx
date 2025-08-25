7689b36bb54feeebbe5d5bec4d8cd91baa250524
Alex <alexis.mar.schmidt@gmail.com>
2025-08-25 14:46:58 +0200

    feat: add login and signup forms

---
+++ src/pages/public/login/LoginForm.tsx
@@ -0,0 +1,57 @@
+import { Form, Input, Button } from '@heroui/react';
+import { useState } from 'react';
+
+export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  console.log('My email: ' + email);
+
+  return (
+    <Form
+      className='w-full max-w-xs'
+      validationBehavior='aria'
+      onSubmit={onSubmit}
+    >
+      <Input
+        isRequired
+        errorMessage='Please enter a valid email'
+        label='Email'
+        labelPlacement='outside'
+        name='email'
+        placeholder='Enter your email'
+        type='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        validate={(value) => {
+          if (!value.includes('@')) {
+            return 'It must be  valid email';
+          }
+        }}
+      />
+      <Input
+        isRequired
+        label='Password'
+        labelPlacement='outside'
+        name='password'
+        placeholder='Enter your password'
+        type='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        validate={(value) => {
+          if (value.length == 0) {
+            return 'Introduce a valid password';
+          }
+        }}
+      />
+
+      <Button type='submit' variant='bordered'>
+        Submit
+      </Button>
+    </Form>
+  );
+}
