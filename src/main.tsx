c81fe7ede00c3acbf11e253207aaaad20887b046
Alex <alexis.mar.schmidt@gmail.com>
2025-08-26 15:33:17 +0200

    feat: add AuthProvider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "@/App.tsx";
 import { HeroUIProvider } from "@heroui/react";
 import { AppRouter } from "@/router/AppRouter";
+import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HeroUIProvider>
       <main className="dark text-foreground bg-background page-layout min-h-screen min-w-screen">
+        <AuthProvider>
         <App>
           <AppRouter />
         </App>
+        </AuthProvider>
       </main>
     </HeroUIProvider>
   </StrictMode>
